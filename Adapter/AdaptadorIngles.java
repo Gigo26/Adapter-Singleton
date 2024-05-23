2142fc0a3ac0e33cc98335c44d5e7827a789b558
Gigo26 <joserex342@gmail.com>
2024-05-23 13:25:06 -0500

    Update AdaptadorIngles.java

--- Adapter/AdaptadorIngles.java
+++ Adapter/AdaptadorIngles.java
@@ -3,6 +3,6 @@ package Adapter;
 public class AdaptadorIngles implements Adaptador {
     @Override
     public String traducir(String mensajeEspanol) {
-        return "I have to go now.";
+        return "Hello, how are you?";
     }
 }
