57738af7e6817c82d9f9f85cbba6c81f56efaae6
Gigo26 <joserex342@gmail.com>
2024-05-23 13:25:33 -0500

    Update AdaptadorQuechua.java

--- Adapter/AdaptadorQuechua.java
+++ Adapter/AdaptadorQuechua.java
@@ -3,6 +3,6 @@ package Adapter;
 public class AdaptadorQuechua implements Adaptador {
     @Override
     public String traducir(String mensajeEspanol) {
-        return "Ya me tenquir.";
+        return "Rimaykullayki, allillanchu kashanki?";
     }
 }
